64b8e5242e8b293a82a4a6b8283d1ff40eb6b834
Marcello <marcellorcestaro@gmail.com>
2020-06-20 10:29:55 -0300

    Adiciona Django CORS Headers

--- app/billwebapp/billwebapp/settings.py
+++ app/billwebapp/billwebapp/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "custom_auth",
     'financas',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,3 +137,7 @@ STATIC_URL = '/static/'
 # Heroku
 
 django_heroku.settings(locals())
+
+# CORS Config
+
+CORS_ORIGIN_ALLOW_ALL = True
