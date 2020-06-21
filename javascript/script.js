bb287388ac788c2eb614055175d0945f7fa3f47f
Caik Moraes <caikdemoraes@gmail.com>
2020-06-21 20:03:09 -0300

    Front dashboard finalizado (falta ajuste da pag Contas)

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,6 @@ const BASE_URL = 'https://billwebapp.herokuapp.com/api/despesa/fixa/'
 let user = {}
 
 window.onload = () => {
-    this.getPage()
     this.getUserInfo()
 }
 
@@ -37,13 +36,30 @@ function compareUserPass() {
 }
 
 function getUserInfo() {
+    let page = this.getPage()
     let url = `${BASE_URL}`
     getJSON(url, function(status, data) {
-        console.log(data[0])
         user = data[0]
+        if (page == 'dashboard') {
+            setUserCash(user)
+            setSalPoup(user)
+        }
+
     })
 }
 
+function setSalPoup(user) {
+    let salario = document.getElementById('txt-salario')
+    let poupanca = document.getElementById('txt-poupanca')
+    salario.value = `R$${Number(user.totalPrevisto).toFixed(2).replace('.', ',')}`
+    poupanca.value = `R$${Number(user.valor).toFixed(2).replace('.', ',')}`
+}
+
+function setUserCash(user) {
+    let cash = document.getElementById('user-cash')
+    let total = user.valor + user.totalPrevisto
+    cash.value = `R$${total.toFixed(2).replace('.',',')}`
+}
 
 function getJSON(url, callback) {
     let xhr = new XMLHttpRequest();
@@ -128,11 +144,107 @@ function upUserPass() {
     document.getElementById('pass-button').style.display = "none"
 }
 
+function dropSpend() {
+    let buttonAdd = document.getElementById('btn-add-spend')
+    let buttonUp = document.getElementById('btn-up-spend')
+    let setSpend = document.getElementById('set-spend')
+    buttonAdd.style.display = 'none'
+    buttonUp.style.display = 'flex'
+    setSpend.style.display = 'block'
+}
+
+function upSpend() {
+    let buttonAdd = document.getElementById('btn-add-spend')
+    let buttonUp = document.getElementById('btn-up-spend')
+    let setSpend = document.getElementById('set-spend')
+    buttonAdd.style.display = 'flex'
+    buttonUp.style.display = 'none'
+    setSpend.style.display = 'none'
+
+}
+
+function dropSalPoupInfo() {
+    let dropIcon = document.getElementById('drop-cash-info')
+    let upIcon = document.getElementById('up-cash-info')
+    let btnSal = document.getElementById('btn-salario')
+    dropIcon.style.display = 'none'
+    upIcon.style.display = 'flex'
+    btnSal.style.display = 'block'
+    this.dropPoup()
+}
+
+function upSalPoupInfo() {
+    let dropIcon = document.getElementById('drop-cash-info')
+    let upIcon = document.getElementById('up-cash-info')
+    let btnSal = document.getElementById('btn-salario')
+    dropIcon.style.display = 'flex'
+    upIcon.style.display = 'none'
+    btnSal.style.display = 'none'
+    this.upPoup()
+}
+
+function dropPoup() {
+    let dropIcon = document.getElementById('drop-poup-info')
+    let upIcon = document.getElementById('up-poup-info')
+    let btnPoup = document.getElementById('btn-poupanca')
+    dropIcon.style.display = 'none'
+    upIcon.style.display = 'flex'
+    btnPoup.style.display = 'block'
+}
+
+function upPoup() {
+    let dropIcon = document.getElementById('drop-poup-info')
+    let upIcon = document.getElementById('up-poup-info')
+    let btnPoup = document.getElementById('btn-poupanca')
+    dropIcon.style.display = 'flex'
+    upIcon.style.display = 'none'
+    btnPoup.style.display = 'none'
+}
+
+function dropMonthInfo() {
+    let dropIcon = document.getElementById('drop-month-info')
+    let upIcon = document.getElementById('up-month-info')
+    let monthSpend = document.getElementById('month-spend')
+    dropIcon.style.display = 'none'
+    upIcon.style.display = 'flex'
+    monthSpend.style.display = 'flex'
+}
+
+function upMonthInfo() {
+    let dropIcon = document.getElementById('drop-month-info')
+    let upIcon = document.getElementById('up-month-info')
+    let monthSpend = document.getElementById('month-spend')
+    dropIcon.style.display = 'flex'
+    upIcon.style.display = 'none'
+    monthSpend.style.display = 'none'
+}
+
+function dropPlanInfo() {
+    let dropIcon = document.getElementById('drop-plan-info')
+    let upIcon = document.getElementById('up-plan-info')
+    let planContent = document.getElementById('plan-content-info')
+    dropIcon.style.display = 'none'
+    upIcon.style.display = 'flex'
+    planContent.style.display = 'flex'
+}
+
+function upPlanInfo() {
+    let dropIcon = document.getElementById('drop-plan-info')
+    let upIcon = document.getElementById('up-plan-info')
+    let planContent = document.getElementById('plan-content-info')
+    dropIcon.style.display = 'flex'
+    upIcon.style.display = 'none'
+    planContent.style.display = 'none'
+}
+
 function getPage() {
     let pagePath = window.location.pathname
     if (pagePath.indexOf('conta.html') >= 0) {
         document.getElementById('acc-link').className += 'active'
+        return 'conta'
+
     } else if (pagePath.indexOf('dashboard.html') >= 0) {
         document.getElementById('dash-link').className += 'active'
+        return 'dashboard'
     }
 }
