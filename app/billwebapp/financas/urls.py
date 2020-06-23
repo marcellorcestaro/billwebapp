fdb01cdbd4aa58f2ea2866fc1190d50a055e11a6
Marcello <marcellorcestaro@gmail.com>
2020-06-22 22:08:16 -0300

    Typo

--- app/billwebapp/financas/urls.py
+++ app/billwebapp/financas/urls.py
@@ -5,7 +5,7 @@ from financas import views
 urlpatterns = [
     path('despesa/variavel/<int:id>/', views.DespesaVariavelDetail.as_view()),
     path('despesa/fixa/<int:id>/', views.DespesaFixaDetail.as_view()),
-    path('ddespesa/adicional/<int:id>/', views.DespesaAdicionalDetail.as_view()),
+    path('despesa/adicional/<int:id>/', views.DespesaAdicionalDetail.as_view()),
     path('renda/<int:id>/', views.RendaDetail.as_view()),
     path('reserva/<int:id>/', views.ReservaDetail.as_view()),
 ]
