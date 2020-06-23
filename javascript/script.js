8d90ac1ee1025019deb9af52f0fbc7f3086fc9bf
Caik Moraes <caikdemoraes@gmail.com>
2020-06-22 22:17:45 -0300

    Refatoração de js

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,11 @@
-const BASE_URL = 'https://billwebapp.herokuapp.com/api/despesa/fixa/'
-let user = {}
+const BASE_URL = 'https://billwebapp.herokuapp.com/api/auth/user/3/'
+const RESERVA_URL = 'https://billwebapp.herokuapp.com/api/reserva/3/'
+const DESPESA_FIXA_URL = 'https://billwebapp.herokuapp.com/api/despesa/fixa/3/'
+const DESPESA_VAR_URL = 'https://billwebapp.herokuapp.com/api/despesa/variavel/3/'
+const DESPESA_ADD_URL = 'https://billwebapp.herokuapp.com/api/despesa/adicional/3/'
 
 window.onload = () => {
-    this.getUserInfo()
+    this.getPage()
 }
 
 // Get current user info
@@ -33,33 +36,21 @@ function getPage() {
 
     } else if (pagePath.indexOf('dashboard.html') >= 0) {
         document.getElementById('dash-link').className += 'active'
+        this.getDashInfo()
+        this.getUserReserve()
+        this.getUserDespesaFixa()
+        this.getMonthSpend()
         return 'dashboard'
     }
 }
 
-
-
-function getUserInfo() {
-    let page = this.getPage()
-    let url = `${BASE_URL}`
-    getJSON(url, function(status, data) {
-        user = data[0]
-        if (page == 'dashboard') {
-            setUserCash(user)
-            setSalPoup(user)
-            setUserName(user)
-        }
-
-    })
-}
-
 //Account page functions
 
-function compareUserEmail() {
+function compareUserEmail(user) {
     let txtUserEmail = document.getElementById('txt-email-atual')
     txtUserEmail.onchange = () => {
         let emailSpan = document.getElementById('email-span')
-        if (txtUserEmail.value != user.categoria) {
+        if (txtUserEmail.value != user.email) {
             emailSpan.style.display = 'block'
         } else {
             emailSpan.style.display = 'none'
@@ -67,11 +58,11 @@ function compareUserEmail() {
     }
 }
 
-function compareUserPass() {
+function compareUserPass(user) {
     let txtUserPass = document.getElementById('txt-pass-atual')
     txtUserPass.onchange = () => {
         let passSpan = document.getElementById('pass-span')
-        if (txtUserPass.value != user.categoria) {
+        if (txtUserPass.value != user.email) {
             passSpan.style.display = 'block'
         } else {
             passSpan.style.display = 'none'
@@ -79,9 +70,9 @@ function compareUserPass() {
     }
 }
 
-function getUserName() {
+function getUserName(user) {
     let txtUserName = document.getElementById('txt-name')
-    txtUserName.placeholder = user.categoria
+    txtUserName.placeholder = `${user.first_name}`
 }
 
 function expandUserName() {
@@ -150,24 +141,105 @@ function upUserPass() {
     document.getElementById('pass-button').style.display = "none"
 }
 
-// Dashboard page funtions
+// Dashboard functions
+
+function getDashInfo() {
+    let url = `${BASE_URL}`
+    let user = {}
+    getJSON(url, function(status, data) {
+        user = data
+        setUserCash(user)
+        setUserName(user)
+        setSal(user)
+    })
+}
+
+function getMonthSpend() {
+    let fixed = getUserDespesaFixa()
+    let add = getUserDespesaAdd()
+    let spendVar = getUserDespesaVar()
+    let monthlySpend = 0
+    for (let x = 0; x < fixed.length; i++) {
+        monthlySpend += fixed[x].valor
+    }
+    for (let y; y < add.length; y++) {
+        monthlySpend += add[y].valor
+    }
+    for (let z; z < add.length; z++) {
+        monthlySpend += spendVar[y].valor
+    }
+    console.log(monthlySpend)
+
+}
+
+function getUserDespesaFixa() {
+    let url = DESPESA_FIXA_URL
+    let fixedSpend = []
+    getJSON(url, function(status, data) {
+        fixedSpend = data
+    })
+    if (fixedSpend == null) {
+        return []
+    }
+    return fixedSpend
+}
+
+function getUserDespesaAdd() {
+    let url = DESPESA_ADD_URL
+    let addSpend = []
+    getJSON(url, function(status, data) {
+        addSpend = data
+    })
+    if (addSpend == null) {
+        return []
+    }
+    return addSpend
+}
+
+function getUserDespesaVar() {
+    let url = DESPESA_VAR_URL
+    let varSpend = []
+    getJSON(url, function(status, data) {
+        varSpend = data
+    })
+    if (varSpend == null) {
+        return []
+    }
+    return varSpend
+}
+
+function getUserReserve() {
+    let url = `${RESERVA_URL}`
+    let user = []
+    getJSON(url, function(status, data) {
+        user = data
+        this.setPoup(user)
+    })
+}
 
 function setUserName(user) {
     let userName = document.getElementById('hello-user')
-    userName.innerHTML = `<h1>Olá ${user.destino}!</h1>`
+    userName.innerHTML = `<h1>Olá ${user.first_name}!</h1>`
 }
 
-function setSalPoup(user) {
+function setSal(user) {
     let salario = document.getElementById('txt-salario')
+    salario.value = `R$${Number(user.profile.rendaMensal).toFixed(2).replace('.', ',')}`
+}
+
+function setPoup(user) {
     let poupanca = document.getElementById('txt-poupanca')
-    salario.value = `R$${Number(user.totalPrevisto).toFixed(2).replace('.', ',')}`
-    poupanca.value = `R$${Number(user.valor).toFixed(2).replace('.', ',')}`
+    let poupValue = 0
+    for (let i = 0; i < user.length; i++) {
+        poupValue += parseFloat(user[i].valor)
+    }
+    poupanca.value = `R$${poupValue}`
 }
 
 function setUserCash(user) {
     let cash = document.getElementById('user-cash')
-    let total = user.valor + user.totalPrevisto
-    cash.innerHTML = `<h3><strong>R$${total.toFixed(2).replace('.',',')}</strong></h3>`
+        // let total = user.valor + user.totalPrevisto
+    cash.innerHTML = `<h3><strong>R$${user.profile.saldoAtual.toFixed(2).replace('.',',')}</strong></h3>`
 }
 
 function dropSpend() {
